83f7b1d525003d649a4d4639126701229a55c9ae
Ariel Altaras <ariel.altaras@gmail.com>
2018-12-02 23:26:25 -0800

    solved part two

--- day3/test.js
+++ day3/test.js
@@ -4,8 +4,8 @@ let input = `#1 @ 1,3: 4x4
 
 let claimArray = input.split('\n');
 
+var pattern = new Map;
 function layout(claims) {
-    let pattern = new Map;
     for (let i = 0; i < claims.length; i++) {
         let parts = claims[i].split(' ');
         let start = parts[2].replace(':', '').split(',');
@@ -36,7 +36,46 @@ function layout(claims) {
         }
     }
     console.log(tally);
+    return pattern;
 
 }
 
-layout(claimArray);
+
+
+function checkClaim(claims) {
+    var allClaims = layout(claimArray);
+    for (let i = 0; i < claims.length; i++) {
+        let thisClaim = new Map;
+        let parts = claims[i].split(' ');
+        let id = parts[0];
+        let start = parts[2].replace(':', '').split(',');
+        let size = parts[3].split('x');
+        let j = parseInt(start[0]);
+        for (j = parseInt(start[0]); j < (parseInt(start[0])+parseInt(size[0])) ; j++) {
+            for (let k = parseInt(start[1]); k < (parseInt(start[1])+ parseInt(size[1])); k++) {
+                let coordinate = [j,k];
+                let key = coordinate.toString();
+                if (thisClaim.has(key)) {
+                    let val = thisClaim.get(key);
+                    thisClaim.set(key, (val += 1));
+                }
+                else {
+                    thisClaim.set(key, 1);
+                }
+}
+        }
+        let keyArr = Array.from(thisClaim.keys());
+        for (let k = 0; k < keyArr.length; k++) {
+            let overlaps = allClaims.get(keyArr[k]);
+            console.log(overlaps);
+            if (overlaps > 1) {
+                k = keyArr.length;
+            }
+            else if (k == keyArr.length -1 && overlaps == 1) {
+                    console.log('ID: ' + id);
+                }
+            }
+        }
+    };
+
+checkClaim(claimArray);
