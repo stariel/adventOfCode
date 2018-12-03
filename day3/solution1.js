dfa17b42df4579e1f39097177065e88c0dcb0366
Ariel Altaras <ariel.altaras@gmail.com>
2018-12-02 22:30:32 -0800

    solved first part

--- day3/solution1.js
+++ day3/solution1.js
@@ -1379,3 +1379,42 @@ let input = `#1 @ 493,113: 12x14
 #1379 @ 971,173: 23x11
 #1380 @ 414,619: 11x17
 #1381 @ 473,735: 25x21`
+
+let claimArray = input.split('\n');
+
+function layout(claims) {
+    let pattern = new Map;
+    for (let i = 0; i < claims.length; i++) {
+        let parts = claims[i].split(' ');
+        let start = parts[2].replace(':', '').split(',');
+        let size = parts[3].split('x');
+        let j = parseInt(start[0]);
+        for (j = parseInt(start[0]); j < (parseInt(start[0])+parseInt(size[0])) ; j++) {
+            for (let k = parseInt(start[1]); k < (parseInt(start[1])+ parseInt(size[1])); k++) {
+                let coordinate = [j,k];
+                let key = coordinate.toString();
+                if (pattern.has(key)) {
+                    let val = pattern.get(key);
+                    pattern.set(key, (val += 1));
+                }
+                else {
+                    pattern.set(key, 1);
+                }
+                
+            }
+            
+        }
+        
+    }
+    let values = Array.from(pattern.values());
+    let tally = 0;
+    for (let i = 0; i < values.length; i++) {
+        if (values[i] > 1) {
+            tally ++;
+        }
+    }
+    console.log(tally);
+
+}
+
+layout(claimArray);
--- day3/test.js
+++ day3/test.js
@@ -5,16 +5,37 @@ let input = `#1 @ 1,3: 4x4
 let claimArray = input.split('\n');
 
 function layout(claims) {
-    console.log(claims);
     let pattern = new Map;
     for (let i = 0; i < claims.length; i++) {
         let parts = claims[i].split(' ');
         let start = parts[2].replace(':', '').split(',');
         let size = parts[3].split('x');
-        console.log(start);
-        console.log(size);
+        let j = parseInt(start[0]);
+        for (j = parseInt(start[0]); j < (parseInt(start[0])+parseInt(size[0])) ; j++) {
+            for (let k = parseInt(start[1]); k < (parseInt(start[1])+ parseInt(size[1])); k++) {
+                let coordinate = [j,k];
+                let key = coordinate.toString();
+                if (pattern.has(key)) {
+                    let val = pattern.get(key);
+                    pattern.set(key, (val += 1));
+                }
+                else {
+                    pattern.set(key, 1);
+                }
+                
+            }
+            
+        }
         
     }
+    let values = Array.from(pattern.values());
+    let tally = 0;
+    for (let i = 0; i < values.length; i++) {
+        if (values[i] > 1) {
+            tally ++;
+        }
+    }
+    console.log(tally);
 
 }
 
